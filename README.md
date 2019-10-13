023e7a9d57f9cf1610acc334e4e3a7d6b9900e6d
Luis Bocaranda <luisbocaranda19@gmail.com>
2019-10-13 17:03:09 -0400

    ultimos cambios

---
+++ README.md
@@ -0,0 +1,7 @@
+pagina que nos ayuda a desplegar nuestra aplicacion:
+    Zeit.co
+
+utilizar: " NOW "
+    que es un paquete de node que nos ayuda a subir nuestra aplicacion para ejecutarla publicamente y puder hacer configuraciones en ella
+
+Buscar informacion sobre Jerocu
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 router.get('/', (req, res)=>{
     res.render('index', { title:'First Website'});  // se utiliza para renderizar el archivo del motor de plantillas y procesar codigo
-
+   // res.render('index.html');
 });
 router.get('/contact', (req, res)=>{
     res.render('contact', { title:'Contact Page'});  // se utiliza para renderizar el archivo del motor de plantillas y procesar codigo
