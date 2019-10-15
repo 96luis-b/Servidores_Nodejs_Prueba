b4147f8d49b2ebefbb2e0988d2f8ce2970e00e06
Luis Bocaranda <luisbocaranda19@gmail.com>
2019-10-14 21:20:17 -0400

    estructura a utilizar en el servidor de  BSTube

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res)=>{
-    res.render('index', { title:'First Website'});  // se utiliza para renderizar el archivo del motor de plantillas y procesar codigo
-   // res.render('index.html');
+    //res.render('index', { title:'First Website'});  // se utiliza para renderizar el archivo del motor de plantillas y procesar codigo
+   res.render('index.html');
 });
 router.get('/contact', (req, res)=>{
     res.render('contact', { title:'Contact Page'});  // se utiliza para renderizar el archivo del motor de plantillas y procesar codigo
